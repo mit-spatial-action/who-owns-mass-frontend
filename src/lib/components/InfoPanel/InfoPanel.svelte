d04e39bf714f486a77f81b71e6fdd70df30f67df
Milan Chuttani <37783536+mchuttani18@users.noreply.github.com>
2024-02-27 12:07:37 -0500

    Hard git reset on store, highlighting on infopanel
    This reverts commit 59a0e9a7c2ba34c9f1071c232cd3dd2716927477.

--- src/lib/components/InfoPanel/InfoPanel.svelte
+++ src/lib/components/InfoPanel/InfoPanel.svelte
@@ -29,13 +29,15 @@
     <p class="block mt-2 is-size-5" on:click={clearState}>
         <span class="has-text-link">&#8592 </span><u>Back</u> to search
     </p>
-
-    <div class="title has-text-dark is-size-1 has-text-left block">
+    <span class="has-text-dark p-1 px-2 is-size-5 has-text-left block has-text-weight-semibold is-uppercase has-background-danger">
+        Corporate Landlord
+    </span>
+    <div class="is-uppercase title has-text-dark is-size-1 has-text-left block">
         {$selectedFeature[0].properties.landlord_name}
     </div>
     <div class="subtitle mt-1 has-text-dark is-size-5 has-text-left block">
         Between September, 2019, and February, 2022, <b>{$selectedFeature[0].properties.landlord_name}</b> was the 
-        cause of approximately <b>{$selectedFeature[0].properties.evictions}</b> people losing their homes or experiencing housing instability.
+        cause of <span class="has-background-warning">approximately <b>{$selectedFeature[0].properties.evictions}</b> people losing their homes or experiencing housing instability.</span>	
     </div>
     <div class="subtitle mt-1 has-text-dark is-size-6 has-text-centered block">
         Eviction Rank: { $selectedFeature[0].properties.eviction_rank } <br/>
