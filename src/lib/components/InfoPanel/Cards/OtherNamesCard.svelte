596d4def3a059ec31fcde30140be87098d0c83fe
anastasia <anastasia.aizman@gmail.com>
2024-07-09 14:30:39 -0400

    add - checking if company is already mentioned in list of related companies

--- src/lib/components/InfoPanel/Cards/OtherNamesCard.svelte
+++ src/lib/components/InfoPanel/Cards/OtherNamesCard.svelte
@@ -1,29 +1,36 @@
 <script>
-    import { selectedFeature } from '$lib/scripts/stores.js';
-    import { onDestroy } from 'svelte';
+    import {
+        selectedFeature,
+        company,
+        getCompany,
+    } from "$lib/scripts/stores.js";
+    import { onDestroy } from "svelte";
+
     /** @type {import('./$types').PageData} */
 
-    const unsubscribe = selectedFeature.subscribe(value => {
-    });
+    const unsubscribe = selectedFeature.subscribe((value) => {});
 
     onDestroy(() => {
         unsubscribe();
     });
-
 </script>
 
 <div>
-    <hr/>
+    <hr />
     <p class="has-text-dark is-size-4 has-text-left block mb-1">
         <b>Also known as</b>
     </p>
-    <div class="has-text-left block"> 
-        {#each JSON.parse($selectedFeature[0].properties.other_names) as item, index (index)}
-            <span class="has-text-link"><u>{item}</u></span>
-            <br/>
-        {/each}     
+    <div class="has-text-left block">
+        {#each $company["metacorp"]["related"]["institutions"] as item, index (index)}
+            {#if item.id != $company.id}
+                <a on:click={$getCompany(id)} class="has-text-link">
+                    <u>{item.name}</u>
+                </a>
+                <br />
+            {/if}
+        {/each}
     </div>
-   <div class="has-text-left block">
-    <span class="has-text-link mb-1" ><u>View corporate addresses</u></span>
-   </div>
+    <div class="has-text-left block">
+        <span class="has-text-link mb-1"><u>View corporate addresses</u></span>
+    </div>
 </div>
