c45cc7539688d4b449834cd058edab2e8fee84a4
anastasia <anastasia.aizman@gmail.com>
2024-07-09 16:06:50 -0400

    add - some css to make things more usable for the moment

--- src/lib/components/InfoPanel/Cards/OtherNamesCard.svelte
+++ src/lib/components/InfoPanel/Cards/OtherNamesCard.svelte
@@ -20,17 +20,30 @@
     <p class="has-text-dark is-size-4 has-text-left block mb-1">
         <b>Also known as</b>
     </p>
-    <div class="has-text-left block">
-        {#each $company["metacorp"]["related"]["institutions"] as item, index (index)}
-            {#if item.id != $company.id}
-                <a on:click={$getCompany(item.id)} class="has-text-link">
-                    <u>{item.name}</u>
-                </a>
-                <br />
-            {/if}
-        {/each}
+    <div class="has-text-left block" id="other-names-box">
+        <ol type="1">
+            {#each $company.metacorp.related.institutions as item, index (index)}
+                {#if item.id != $company.id}
+                    <li>
+                        <a
+                            on:click={$getCompany(item.id)}
+                            class="has-text-link"
+                        >
+                            <u>{item.name}</u>
+                        </a>
+                    </li>
+                {/if}
+            {/each}
+        </ol>
     </div>
     <div class="has-text-left block">
         <span class="has-text-link mb-1"><u>View corporate addresses</u></span>
     </div>
 </div>
+
+<style>
+    #other-names-box {
+        max-height: 200px;
+        overflow-y: scroll;
+    }
+</style>
