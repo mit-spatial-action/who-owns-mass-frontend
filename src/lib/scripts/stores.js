67dd4335583ac679696196e204a03e4f6300d76b
anastasia <anastasia.aizman@gmail.com>
2024-07-09 15:27:00 -0400

    bump - added storejs getcompany function

--- src/lib/scripts/stores.js
+++ src/lib/scripts/stores.js
@@ -8,3 +8,22 @@ export const getMap = writable(() => { });
 
 export const company = writable({});
 
+export const getCompany = readable(async (id) => {
+    console.log("getCompany in storejs");
+    await fetch("api/companies/" + id + "/", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("get company in storejs", data)
+            company.set(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            return [];
+        })
+});
+
