0c9a459465bb794bec8deeabcc2fcf052b6e7cd0
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-11-06 09:57:09 -0500

    generate redirects after build

--- generate-redirects.mjs
+++ generate-redirects.mjs
@@ -1,5 +1,5 @@
 // generate-redirects.js
-import { writeFile, mkdir } from 'fs/promises';
+import { appendFile } from 'fs/promises';
 
 // Read the API URL from the environment variable
 const apiUrl = process.env.VITE_PUBLIC_API_URL;
@@ -14,8 +14,7 @@ const redirectsContent = `/api/* ${apiUrl}/:splat 200`;
 
 // Write the redirects content to the _redirects file in the build directory
 try {
-  await mkdir('build', { recursive: true });
-  await writeFile('build/_redirects', redirectsContent, 'utf8');
+  await appendFile('build/_redirects', redirectsContent, 'utf8');
   console.log("Redirects file generated with API URL:", apiUrl);
 } catch (error) {
   console.error("Failed to write redirects file:", error);
