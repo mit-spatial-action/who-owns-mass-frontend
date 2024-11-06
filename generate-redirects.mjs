56f8e737f80346f0b35d8cccb3ad678730e79015
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-11-06 10:52:11 -0500

    try prepending to build/_redirects

--- generate-redirects.mjs
+++ generate-redirects.mjs
@@ -1,5 +1,5 @@
 // generate-redirects.js
-import { appendFile } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 
 // Read the API URL from the environment variable
 const apiUrl = process.env.VITE_PUBLIC_API_URL;
@@ -10,13 +10,13 @@ if (!apiUrl) {
 }
 
 // Define the redirect rule with the API URL included
-const redirectsContent = `\n/api/* ${apiUrl}/:splat 200!`;
-const redirectsContent2 = `\n/test https://www.example.com/ 200!`;
+const redirectsContent = `/api/* ${apiUrl}/:splat 200!\n`;
 
 // Write the redirects content to the _redirects file in the build directory
 try {
-  await appendFile('build/_redirects', redirectsContent, 'utf8');
-  await appendFile('build/_redirects', redirectsContent2, 'utf8');
+  const contents = await readFile('build/_redirects', 'utf8');
+  const newContents = redirectsContent + contents;
+  await writeFile('build/_redirects', newContents)
   console.log("Redirects file generated with API URL:", apiUrl);
 } catch (error) {
   console.error("Failed to write redirects file:", error);
