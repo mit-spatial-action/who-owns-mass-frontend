0ee3665c2ba587327d075db2daf3816114b3b864
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-11-13 13:24:59 -0500

    animate circles

--- src/routes/site/[id]/+page.svelte
+++ src/routes/site/[id]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import SitePanel from "$lib/components/Panels/SitePanel.svelte";
-	import { site, metacorp, homeState } from "$lib/scripts/stores";
+	import { site, metacorp, homeState } from "$lib/stores";
   	/** @type {{ data: import('./$types').PageData }} */
 	let { data } = $props();
 	$effect(() => {
