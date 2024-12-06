617811e47e2466484818438d42e62c1409c410ff
Eric Robsky Huntley <ericrobskyhuntley@gmail.com>
2024-12-06 09:41:38 -0500

    implement token authentication on api calls

--- src/routes/site/[id]/+page.server.ts
+++ src/routes/site/[id]/+page.server.ts
@@ -1,10 +1,11 @@
 import { getFromApi } from "$lib/scripts/utils";
 import type { PageLoad } from './$types';
+import { API_TOKEN } from '$env/static/private';
 
 export const load: PageLoad = async ({ params, fetch }) => {
     const { id } = params;
-    const metaCorpData = await getFromApi(fetch, '/api', 'metacorps', id);
+    const siteData = await getFromApi(fetch, '/api', API_TOKEN, 'sites', id);
     return { 
-        metaCorpData 
+        siteData 
     }
 };
