4ef6003da90a7885aec0af4bb1ccc215a3c1a86f
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2025-07-29 12:42:00 -0400

    #146 wrap simplification of panels

--- src/routes/meta/[id]/+page.server.ts
+++ src/routes/meta/[id]/+page.server.ts
@@ -4,12 +4,12 @@ import { API_TOKEN } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
     const { id } = params;
-    const metaCorp = await getFromApi(fetch, '/api', API_TOKEN, 'metacorps', id);
+    const metacorp = await getFromApi(fetch, '/api', API_TOKEN, 'metacorps', id);
     return { 
-        metaCorp,
+        metacorp,
         seo: {
-			title: metaCorp.name,
-			description: `We found up to ${metaCorp.unit_count} units and ${metaCorp.prop_count} properties owned by ${metaCorp.name}`
+			title: metacorp.name,
+			description: `We found up to ${metacorp.unit_count} units and ${metacorp.prop_count} properties owned by ${metacorp.name}`
 		}
     }
 };
