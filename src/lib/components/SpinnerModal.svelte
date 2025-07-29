34ce8af1d433f2d5720c3a404c5f1f9ffc197bb6
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2025-07-29 11:38:14 -0400

    #146 simp `RippleLoader`>`svelte-loading-spinners`

---
+++ src/lib/components/SpinnerModal.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { fade, fly } from "svelte/transition";
+    import { Circle2 } from "svelte-loading-spinners";
+    import { primary, success } from '$lib/styles/_variables';
+</script>
+
+<div class="modal is-active" transition:fade={{ duration: 400 }}>
+    <div class="modal-background"></div>
+        <div transition:fly={{ duration: 200, y: 200 }}>
+            <Circle2
+                size="100"
+                colorOuter={success}
+                colorCenter={primary}
+                colorInner={success}
+                unit="px"
+                durationOuter="2s"
+                durationCenter="1.5s"
+                durationInner="1.75s"
+            />
+        </div>
+</div>
+<style lang="scss">
+    .modal-background {
+        opacity: 0.68;
+    }
+</style>
