b10e915bcec779042ca4b851289d93e188aae953
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2025-08-01 15:21:03 -0400

    #112 fix: external link

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -8,36 +8,43 @@
 </script>
 
 {#if appState.sidebar}
-<div
-  id="sidebar"
-  class="has-background p-3"
-  transition:fly={{ x: -350, duration: 300 }}
->
-  <aside class="menu">
-    <div class="buttons is-small">
-      <button
-        class="button border-success is-responsive"
-        onclick={() => (appState.sidebar = false)}
-        aria-label="Close sidebar"
-      >
-        <span class="icon">
-          <i class="fa-solid fa-x"></i>
-        </span>
-      </button>
-    </div>
-    <p class="menu-label">{siteConfig.title}</p>
-    <ul class="menu-list">
-      {#each menuItems as item}
-        <li>
-          <a
-            href={item[Object.keys(item)[0]]}
-            onclick={() => (appState.sidebar = false)}>{Object.keys(item)[0]}</a
-          >
-        </li>
-      {/each}
-    </ul>
-  </aside>
-</div>
+  <div
+    id="sidebar"
+    class="has-background p-3"
+    transition:fly={{ x: -350, duration: 300 }}
+  >
+    <aside class="menu">
+      <div class="buttons is-small">
+        <button
+          class="button border-success is-responsive"
+          onclick={() => (appState.sidebar = false)}
+          aria-label="Close sidebar"
+        >
+          <span class="icon">
+            <i class="fa-solid fa-x"></i>
+          </span>
+        </button>
+      </div>
+      <p class="menu-label">{siteConfig.title}</p>
+      <ul class="menu-list">
+        {#each menuItems as item}
+          <li>
+            <a
+              href={item[Object.keys(item)[0]]}
+              onclick={() => (appState.sidebar = false)}
+            >
+              {Object.keys(item)[0]}
+              {#if item[Object.keys(item)[0]].includes("http")}
+                <span class="icon">
+                  <i class="fas fa-external-link"></i>
+                </span>
+              {/if}
+            </a>
+          </li>
+        {/each}
+      </ul>
+    </aside>
+  </div>
 {/if}
 
 <style lang="scss">
